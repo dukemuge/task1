6ad2feacc3a8cdc38e00cc7b69cf3a47a6ad6843
Geoffrey Sagini <saginigeoffrey@gmail.com>
2023-03-12 16:10:06 +0300

    added commandline Runner

--- src/main/java/com/pezesha/task/TaskApplication.java
+++ src/main/java/com/pezesha/task/TaskApplication.java
@@ -1,9 +1,13 @@
 package com.pezesha.task;
 
+import com.pezesha.task.models.User;
+import com.pezesha.task.repositories.UserRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class TaskApplication {
@@ -16,6 +20,16 @@ public class TaskApplication {
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
+	}
+
+
+	//Test spring authentication for Testing purpose
+	@Bean
+	CommandLineRunner commandLineRunner(UserRepository users, PasswordEncoder encoder) {
+		return args -> {
+			users.save(new User(1L,"user",encoder.encode("password"),"ROLE_USER"));
+			users.save(new User(2L,"admin",encoder.encode("password"),"ROLE_USER,ROLE_ADMIN"));
+		};
 	}
 
 }
